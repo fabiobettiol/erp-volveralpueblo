35ee1eccfb8113684863888734fdef7f857d8fa2
Fabio Bettiol <fabiobettiol@gmail.com>
2024-01-31 09:25:34 +0100

    WIP: Stats dashboard

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,10 +2,15 @@
 
 namespace App\Nova\Dashboards;
 
+use App\Models\Family;
 use Laravel\Nova\Cards\Help;
 use App\Nova\Metrics\Demandants;
+use Illuminate\Support\Facades\Auth;
 use App\Nova\Metrics\DemandantsFollowups;
+use Stepanenko3\NovaCards\Cards\HtmlCard;
 use Laravel\Nova\Dashboards\Main as Dashboard;
+use Coroowicaksono\ChartJsIntegration\StackedChart;
+
 
 class Main extends Dashboard
 {
@@ -16,10 +21,23 @@ class Main extends Dashboard
      */
     public function cards()
     {
-        return [
-            // // new Help,
-            // (new Demandants)->width('1/4'),
-            // (new DemandantsFollowups)->width('1/4'),
-        ];
+
+        // $thisCdr = Auth::user()->cdr_id;
+        // $thisYear = now()->year;
+
+
+        // $numFamilies = Family::where('cdr_id', 22)
+        //     ->whereYear('settlementdate', 2024)
+        //     ->count();
+
+        // return [
+        //     (new HtmlCard)
+        //         ->width('1/3')
+        //         ->markdown('# Hello World!'), // Required
+
+        //     (new HtmlCard)
+        //         ->width('1/3')
+        //         ->view('cards.families', ['count' => $numFamilies]),
+        // ];
     }
 }
